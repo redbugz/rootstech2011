a124ba401d776c49da8a5936aa4f52bbe831b713
Logan Allred <redbugz@gmail.com>
2011-02-10 03:33:34 -0700

    add config.php file with generic config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$developer_key = "NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN-NNNN"; # FamilySearch Developer Key
+$callback_url = "http://loganallred.com/rootstech2011/oauth.php"; # Set this to the URL from which you are running the oauth.php script
+$api_host = "http://www.dev.usys.org"; # https://api.familysearch.org if working with production system
+
+?>
